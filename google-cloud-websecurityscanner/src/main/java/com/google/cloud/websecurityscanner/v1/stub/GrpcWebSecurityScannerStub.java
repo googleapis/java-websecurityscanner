65d996f355158d164fb0c858ed13fb11f292b29e
yoshi-automation <yoshi-automation@google.com>
2021-08-05 02:15:01 -0700

    chore: release gapic-generator-java v1.0.17
    Committer: @miraleung
    PiperOrigin-RevId: 388499329
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Aug 3 11:12:29 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: bb0a090d9204110042ab5dee2ce9e06e8071ce54
    Source-Link: https://github.com/googleapis/googleapis/commit/bb0a090d9204110042ab5dee2ce9e06e8071ce54

--- google-cloud-websecurityscanner/src/main/java/com/google/cloud/websecurityscanner/v1/stub/GrpcWebSecurityScannerStub.java
+++ google-cloud-websecurityscanner/src/main/java/com/google/cloud/websecurityscanner/v1/stub/GrpcWebSecurityScannerStub.java
@@ -596,7 +596,13 @@ public class GrpcWebSecurityScannerStub extends WebSecurityScannerStub {
 
   @Override
   public final void close() {
-    shutdown();
+    try {
+      backgroundResources.close();
+    } catch (RuntimeException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new IllegalStateException("Failed to close resource", e);
+    }
   }
 
   @Override
